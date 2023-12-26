d50f602048bea03d1bf2ffaa01d29c52b7df53af
zou_hong_run <zhr19853149156@163.com>
2023-12-26 22:53:45 +0800

    feat: 初始化项目

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(env.REDRUN_ADMIN);
+  return {
+    plugins: [vue()],
+  };
+});
